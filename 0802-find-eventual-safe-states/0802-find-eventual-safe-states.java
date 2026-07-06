654d74d198120bb3aadb2646ccdb5e0db33c9292
geetika-kathpal836 <geetikakathpal77@gmail.com>
2026-07-06 17:16:28 +0530

    Time: 27 ms (32.67%), Space: 58 MB (98.46%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,33 +1,35 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int[] vis=new int[graph.length];
-        int[] pathvis=new int[graph.length];
-        int[] check=new int[graph.length];
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        int[] indegree=new int[graph.length];
         for(int i=0;i<graph.length;i++){
-            if(vis[i]==0){
-                dfscheck(i,vis,pathvis,check,graph);
-            }
+            adj.add(new ArrayList<Integer>());
         }
-        List<Integer> safenodes=new ArrayList<>();
         for(int i=0;i<graph.length;i++){
-            if(check[i]==1){
-                safenodes.add(i);
+            for(int it:graph[i]){
+                adj.get(it).add(i);
+                indegree[i]++;
             }
         }
-        return safenodes;
-    }
-    private boolean dfscheck(int node, int[]vis, int[] pathvis, int[] check,int[][] graph){
-        vis[node]=1;
-        pathvis[node]=1;
-        check[node]=0;
-        for(int it:graph[node]){
-            if(vis[it]==0){
-                if(dfscheck(it,vis,pathvis,check,graph)==true) return true;
+        Queue<Integer> q=new LinkedList<>();
+        List<Integer> ans=new LinkedList<>();
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0){
+                q.offer(i);
             }
-            else if(pathvis[it]==1) return true;
         }
-        check[node]=1;
-        pathvis[node]=0;
-        return false;
+        while(!q.isEmpty()){
+            int node=q.poll();
+            ans.add(node);
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.offer(it);
+                }
+            }
+            
+        }
+        Collections.sort(ans);
+        return ans;
     }
 }
