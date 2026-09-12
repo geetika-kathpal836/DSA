be4f9b3bce6f76fb19c387dc52f74e32eabeae4b
geetika-kathpal836 <geetikakathpal77@gmail.com>
2026-09-12 22:24:28 +0530

    Time: 7 ms (89.16%), Space: 46.3 MB (38.2%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -11,10 +11,6 @@ class Solution {
             while(r-l+1-maxf>k){
                 hash[s.charAt(l)-'A']--;
                 l++;
-                maxf=0;
-                for(int i=0;i<25;i++){
-                    maxf=Math.max(maxf,hash[i]);
-                }
             }
             maxlen=Math.max(maxlen,r-l+1);
             r++;
